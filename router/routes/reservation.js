fd64dc13b2e2ece72e1accaa16891d9b1eb30e3c
Tom van den Berg <tomvandenberg11@hotmail.com>
2022-06-22 11:46:04 +0200

    securing reservation

--- router/routes/reservation.js
+++ router/routes/reservation.js
@@ -1,8 +1,11 @@
 import {reservationsCollection} from './../../modules/mongoClient.js'
 
 export const reservationRoute = async (req, res) => {
-  console.log(req.body);
-  reservationsCollection.insertOne(req.body)
+  const reservation = req.body
+  const randomCode = Math.floor(1000 + Math.random() * 9000)
+  reservation.code = randomCode
+
+  reservationsCollection.insertOne(reservation)
     .then(() => res.redirect('/successful'))
     .catch(error => console.error(error))
 }
