553878592d790d36e915b975a395e0504f9c25e1
Tom van den Berg <tomvandenberg11@hotmail.com>
2022-06-22 12:40:02 +0200

    error message if code is wrong

--- router/routes/deleteRoute.js
+++ router/routes/deleteRoute.js
@@ -4,9 +4,7 @@ export const deleteRoute = async (req, res) => {
     const code = req.body.code
     console.log(code)
 
-    reservationsCollection.remove({
-            'code': code
-        })
+    reservationsCollection.remove({ 'code': code })
         .then(() => {
             res.render('manage', {
                 title: 'Manage your reservation',
