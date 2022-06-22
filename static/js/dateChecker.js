bf454cf3c939c78a17b83608ca189f7d11cc544a
maggness <timderoller@gmail.com>
2022-06-22 11:57:13 +0200

    changed day

--- static/js/dateChecker.js
+++ static/js/dateChecker.js
@@ -5,7 +5,7 @@ let dd = String(todayDate.getDate()).padStart(2, '0');
 let mm = String(todayDate.getMonth() + 1).padStart(2, '0'); //January is 0!
 let yyyy = todayDate.getFullYear();
 
-let todayDateFull = mm + '/' + dd + '/' + yyyy;
+let todayDateFull = dd + '/' + mm + '/' + yyyy;
 
 placeDateValue.value = todayDateFull;
 
